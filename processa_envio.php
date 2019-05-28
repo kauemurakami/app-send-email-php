b289dcf2038a5cf3df8c863aa915c9a12047f118
kauê murakami <kauetmurakami@gmail.com>
2019-05-28 17:52:28 -0300

    finalizando api,

--- processa_envio.php
+++ processa_envio.php
@@ -14,7 +14,7 @@ require "./bibliotecas/PHPMailer/POP3.php";
 		private $para = null;
 		private $assunto = null;
 		private $mensagem = null;
-
+		public $status = array('codigo_status'=> null, 'descricai_status' => '');
 		public function __get($atributo){
 			return $this->$atributo;
 		}
@@ -36,16 +36,16 @@ require "./bibliotecas/PHPMailer/POP3.php";
 	$mensagem->__set('assunto',$_POST['assunto']);
 	$mensagem->__set('mensagem',$_POST['mensagem']);
 
-	print_r($mensagem);
-
 	if(!$mensagem->mensagemValida()){
-		die();
+		header('Location : index.php');
+
 	}
 
 		$mail = new PHPMailer(true);
 		try {
 		    //Server settings
-		    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+		    #$mail->SMTPDebug = 2;// Enable verbose debug output
+		    $mail->SMTPDebug = false;
 		    $mail->isSMTP();                                      // Set mailer to use SMTP
 		    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 		    $mail->SMTPAuth = true;                               // Enable SMTP authentication
@@ -73,10 +73,54 @@ require "./bibliotecas/PHPMailer/POP3.php";
 		    $mail->AltBody = 'é necessário utilizar um client que suport html para ter acesso total ao conteudo da mensagem';
 
 		    $mail->send();
-		    echo 'Email enviado com sucesso';
+
+		    $mensagem->status['codigo_status'] = 1;
+		    $mensagem->status['descricao_status'] = 'Mensagem enviada com sucesso';
 		} catch (Exception $e) {
-			echo 'Message could not be sent.';
-			echo 'Mailer Error: ' . $mail->ErrorInfo;
+		    $mensagem->status['codigo_status'] = 2;
+		    $mensagem->status['descricao_status'] = 'Não foi possivel enviar o email' . $mail->ErrorInfo;
 		}
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	
+		<meta charset="utf-8" />
+    	<title>App Mail Send</title>
+
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+<body>
+	<div class="container">
+			<div class="py-3 text-center">
+				<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+				<h2>Send Mail</h2>
+				<p class="lead">Seu app de envio de e-mails particular!</p>
+			</div>
+		<div class="row">
+			<div class="col-md-12">
+				<? if ($mensagem->status['codigo_status'] == 1) { ?>
+					<div class=" container">
+						<h1 class="display-4 text-success">Sucesso </h1>
+						<p><?= $mensagem->status['descricao_status'] ?></p>
+						<a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+					</div>
+				<? } ?>
+
+				<? if ($mensagem->status['codigo_status'] == 2) { ?>
+					<div class=" container">
+						<h1 class="display-4 text-danger">Erro</h1>
+						<p><?= $mensagem->status['descricao_status'] ?></p>
+						<a href="index.php" class="btn btn-danger btn-lg mt-5 text-white">Voltar</a>
+					</div>
+
+
+				<? } ?>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
